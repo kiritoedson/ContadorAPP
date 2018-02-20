b55d0b888d9cc955f4f49b3083f50c20907ffe82
kiritoedson <edson_beatle@hotmail.com>
2018-02-20 16:34:18 -0600

    Nuevo

---
+++ app/src/main/java/com/example/edson/contadorapp/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.edson.contadorapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    public Button incrementa,quita,reset;
+    public TextView texto;
+    public int conteo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        incrementa=findViewById(R.id.Incre);
+        quita=findViewById(R.id.Quita);
+        reset=findViewById(R.id.Reset);
+        texto=findViewById(R.id.TxtView);
+        conteo=0;
+    }
+
+    public void incrementa (){
+        conteo++;
+        muetra();
+    }
+
+    public void quitar (){
+        conteo--;
+        muetra();
+    }
+
+    public void reset (){
+        conteo=0;
+        muetra();
+    }
+
+    public void muetra(){
+        texto.setText("Conteo: "+conteo);
+
+    }
+}
