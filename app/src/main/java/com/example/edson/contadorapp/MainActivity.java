7fd4f0aed0087aaed9e82729d03b0de690159f75
kiritoedson <edson_beatle@hotmail.com>
2018-02-20 16:37:26 -0600

    eventos botones

--- app/src/main/java/com/example/edson/contadorapp/MainActivity.java
+++ app/src/main/java/com/example/edson/contadorapp/MainActivity.java
@@ -7,7 +7,7 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    public Button incrementa,quita,reset;
+    public Button incrementa, quita, reset;
     public TextView texto;
     public int conteo;
 
@@ -16,30 +16,52 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        incrementa=findViewById(R.id.Incre);
-        quita=findViewById(R.id.Quita);
-        reset=findViewById(R.id.Reset);
-        texto=findViewById(R.id.TxtView);
-        conteo=0;
+        incrementa = findViewById(R.id.Incre);
+        quita = findViewById(R.id.Quita);
+        reset = findViewById(R.id.Reset);
+        texto = findViewById(R.id.TxtView);
+        conteo = 0;
+
+        incrementa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                incrementa();
+            }
+        });
+
+        quita.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                quitar();
+            }
+        });
+
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                reset();
+
+            }
+        });
     }
 
-    public void incrementa (){
+    public void incrementa() {
         conteo++;
         muetra();
     }
 
-    public void quitar (){
+    public void quitar() {
         conteo--;
         muetra();
     }
 
-    public void reset (){
-        conteo=0;
+    public void reset() {
+        conteo = 0;
         muetra();
     }
 
-    public void muetra(){
-        texto.setText("Conteo: "+conteo);
+    public void muetra() {
+        texto.setText("Conteo: " + conteo);
 
     }
 }
